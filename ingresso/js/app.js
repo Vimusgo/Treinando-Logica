0a503dcebf21932b73e940f1cff5760ca90a63cd
Vimusgo <vns.amaralsan@gmail.com>
2025-10-23 16:32:33 -0300

    atualizado exibição de alertas criado readme explicando meu processo de criação

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -2,26 +2,33 @@ let pistaIngressos = 100;
 let superiorIngressos = 200;
 let inferiorIngressos = 400;
 
+atualizarIgressosDisponivel();
+
 function comprar() {
     
     let ingresso = document.getElementById('tipo-ingresso').value;
     let quantidade = document.getElementById('qtd').value;
+    let comprado = true;
 
-    if ( ingresso === 'inferior' && quantidade <= inferiorIngressos ) {
+    if ( quantidade <= 0) {
+        comprado = false;
+    } else if ( ingresso === 'inferior' && quantidade <= inferiorIngressos ) {
         inferiorIngressos = inferiorIngressos - quantidade;
     } else if ( ingresso === 'superior' && quantidade <= superiorIngressos) {
         superiorIngressos = superiorIngressos - quantidade;
     } else if ( ingresso === 'pista' && quantidade <= pistaIngressos) {
         pistaIngressos = pistaIngressos - quantidade;
     } else {
-        exibirAlerta(ingresso);
+        comprado = false;
     }
 
-    console.log (`Quantidade de ingressos: Pista ${pistaIngressos}, Superior ${superiorIngressos}, Inferior ${inferiorIngressos}`)
+    exibirAlerta(ingresso,quantidade,comprado);
+
+    atualizarIgressosDisponivel();
 
 }
 
-function exibirAlerta(i) {
+function exibirAlerta(i,q,c) {
 
     let nome;
 
@@ -36,10 +43,24 @@ function exibirAlerta(i) {
         nome = 'Pista';
     }
     
-    if (i > 0) {
-        alert(`Não foi possivel concluir a compra pois restam apenas ${i} ingressos na ${nome}, altere a quantidade desejada.`);
+    if ( c === true ) {
+        alert(`Compra de ${q} ingressos na categoria ${nome} efetuado com sucesso! :)`);
     } else {
-        alert(`Ingressos da categoria ${nome} estão esgotados.`);
+        if (q <= 0) {
+            alert(`Quantidade invalida!`);
+        } else if (i > 0) {
+            alert(`Não foi possivel concluir a compra pois restam apenas ${i} ingressos na ${nome}, altere a quantidade desejada.`);
+        } else {
+            alert(`Ingressos da categoria ${nome} estão esgotados.`);
+        }
     }
+
+}
+
+function atualizarIgressosDisponivel() {
+
+    document.getElementById('qtd-pista').textContent = `${pistaIngressos}`;
+    document.getElementById('qtd-superior').textContent = `${superiorIngressos}`;
+    document.getElementById('qtd-inferior').textContent = `${inferiorIngressos}`;
 
 }
