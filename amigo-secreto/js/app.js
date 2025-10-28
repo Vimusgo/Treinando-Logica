3e641a6ff251f980191aaad0c43c80fdfb44f696
Vimusgo <vns.amaralsan@gmail.com>
2025-10-28 18:28:55 -0300

    criado função do botão adicionar criado função do botão sortear criado função do botão reiniciar criado limitadores de input invalido e sorteio criado função para excluir nomes da lista

---
+++ amigo-secreto/js/app.js
@@ -0,0 +1,149 @@
+let listaAmigos = ['Vinicius', 'Gaerica', 'Repolho', 'Morpheus'];
+let amigoSecretos = [];
+let checagemSorteio = false;
+
+function adicionar() {
+
+    if (checagemSorteio == true) {
+
+        limpaSorteio();
+        adicionarAmigo();
+
+    } else if (checagemSorteio == false) {
+
+        adicionarAmigo();
+
+    } else {
+        
+        amigoSecretos = [];
+        document.getElementById ('lista-sorteio').innerHTML = '';
+        adicionarAmigo();
+
+    }
+
+}
+
+function adicionarAmigo() {
+
+    let nome = document.getElementById('nome-amigo').value;
+
+    if (nome === '') {
+
+        alert ('Ops, você não digitou um nome, tente novamente.');
+
+    } else if (listaAmigos.includes(nome)){
+
+        alert ('Você colocou um nome repetido, adicione um apelido ou o sobrenome para não confudir no resultado do sorteio.');
+
+    } else {
+
+        listaAmigos.push(nome);
+        console.log (listaAmigos);
+        exibirListaAmigos();
+        
+        document.getElementById('nome-amigo').value = '';
+
+    }
+
+}
+
+function sortear() {
+
+    let quantidade = listaAmigos.length;
+
+    if (quantidade <= 2) {
+
+        alert('Não há pessoas o suficiente para realizar um sorteio.');
+
+    } else if (checagemSorteio == false) {
+
+        for (let i = 0; i < quantidade; i++) {
+
+            let amigoSorteado = sorteador(quantidade); 
+
+            while (amigoSecretos.includes(amigoSorteado) || i === amigoSorteado) {
+                amigoSorteado = sorteador(quantidade);
+            }
+
+        amigoSecretos.push(amigoSorteado);
+        console.log(amigoSecretos);    
+        }
+
+        exibirListaAmigosSecretos(quantidade);
+
+        checagemSorteio = true;
+
+    } else {
+
+        alert ('Sorteio já foi efetuado, clique em reiniciar para fazer um novo sorteio.');
+
+    }
+
+}
+
+function reiniciar() {
+
+    listaAmigos = [];
+    document.getElementById ('lista-amigos').textContent = '';
+    limpaSorteio();
+
+
+}
+
+function limpaSorteio() {
+
+    document.getElementById ('lista-sorteio').innerHTML = '';
+    amigoSecretos = [];
+    checagemSorteio = false;
+
+}
+
+function sorteador(q) {
+
+    return Math.floor(Math.random() * (q));
+
+}
+
+function exibirListaAmigos() {
+
+    //Solução que eu fiz no inicio do projeto:
+    //document.getElementById('lista-amigos').textContent = `${listaAmigos.join(', ')}.`;
+
+    //Um desafio que não consegui concluir, mas achei tão legal a resolução que puxei ela pro projeto e fiz algumas alterações visuais:
+    let lista = document.getElementById('lista-amigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < listaAmigos.length; i++) {
+
+        // Cria um elemento de parágrafo para cada amigo
+        let paragrafo = document.createElement('span');
+        paragrafo.textContent = ' [' + listaAmigos[i] + '] ';
+       
+        // Adiciona um evento de clique para excluir o amigo
+        paragrafo.addEventListener('click', function() {
+            excluirAmigo(i);
+        });
+
+
+        // Adiciona o parágrafo à lista
+        lista.appendChild(paragrafo);
+
+    }
+
+}
+//Parte da função do desafio, que adiciona a possibilidade de excluir itens da lista clicando no elemento do html:
+function excluirAmigo(index) {
+    listaAmigos.splice(index, 1);
+    exibirListaAmigos();
+    limpaSorteio();
+}
+
+function exibirListaAmigosSecretos(q) {
+
+    let lista = document.getElementById('lista-sorteio');
+
+    for (i = 0; i < q; i++) {
+        lista.innerHTML = lista.innerHTML + listaAmigos[i] + ' --> ' + listaAmigos[amigoSecretos[i]] + '<br>';
+    }
+
+}
